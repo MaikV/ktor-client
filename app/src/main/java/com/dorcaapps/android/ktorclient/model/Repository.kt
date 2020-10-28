0d711c338054a14b959f5b5c483b29ee3a915f41
MaikV <scheik@gmx.de>
2020-10-28 23:40:46 +0100

    Added method for uploading files in cache to server. Authentication on server broke Glide's image download

--- app/src/main/java/com/dorcaapps/android/ktorclient/model/Repository.kt
+++ app/src/main/java/com/dorcaapps/android/ktorclient/model/Repository.kt
@@ -1,5 +1,6 @@
 package com.dorcaapps.android.ktorclient.model
 
+import android.content.Context
 import android.net.Uri
 import androidx.core.net.toUri
 import androidx.paging.Pager
@@ -7,19 +8,26 @@ import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.dorcaapps.android.ktorclient.ui.paging.MediaData
 import com.dorcaapps.android.ktorclient.ui.paging.MediaPagingSource
+import dagger.hilt.android.qualifiers.ApplicationContext
 import io.ktor.client.*
 import io.ktor.client.features.*
 import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.util.cio.*
 import io.ktor.utils.io.*
+import io.ktor.utils.io.core.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.flow.*
 import java.io.File
 import javax.inject.Inject
 
-class Repository @Inject constructor(private val client: HttpClient) {
+class Repository @Inject constructor(
+    private val client: HttpClient,
+    @ApplicationContext private val context: Context
+) {
 
     fun getFileUri(id: Int): Flow<Resource<Uri>> = flow<Resource<Uri>> {
         val response = client.get<HttpResponse>(path = "media/$id").throwOnError()
@@ -48,16 +56,34 @@ class Repository @Inject constructor(private val client: HttpClient) {
         ).flow
     }
 
+    suspend fun uploadFilesInCache() {
+        val files = context.cacheDir.listFiles() ?: return
+        val contentType = ContentType.Video.Any
+        for (file in files) {
+            flow<Unit> {
+                val myFormData = formData {
+                    append(
+                        "test",
+                        file.name,
+                        contentType
+                    ) {
+                        writeFully(file.readBytes())
+                    }
+                }
+
+                val result = client.submitFormWithBinaryData<HttpResponse>(
+                    formData = myFormData,
+                    path = "media"
+                ) {
+                    this.method = HttpMethod.Post
+                }.throwOnError()
+            }.addRetryWithLogin().launchIn(MainScope())
+        }
+    }
+
     suspend fun login() {
         client.get<Unit>(path = "login")
     }
-
-//    private suspend fun getMediaPage(page: Int, pageSize: Int): List<MediaData> =
-//        client.get(path = "media") {
-//            parameter("page", page)
-//            parameter("pageSize", pageSize)
-//            parameter("order", OrderType.MOST_RECENT_FIRST)
-//        }
 
     private suspend fun getMediaPage(page: Int, pageSize: Int) = flow<List<MediaData>> {
         emit(client.get(path = "media") {
